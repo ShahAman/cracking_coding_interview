1d1e9ad4780c1b0e99387de3aea3d92b47d246dd
Shahnewaz <shahnewaz@sgcsoft.net>
2023-08-07 22:19:05 +0600

    Added assignment on array

---
+++ 02_Assignment_on_Array.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int getMaxProfit(vector<int> &prices)
+{
+    int minPrice = 104;
+    int maxProfit = 0;
+
+    for (int i = 0; i < prices.size(); ++i)
+    {
+        if (prices[i] < minPrice)
+        {
+            minPrice = prices[i];
+        }
+        else if (prices[i] - minPrice > maxProfit)
+        {
+            maxProfit = prices[i] - minPrice;
+        }
+    }
+
+    return maxProfit;
+}
+
+int main()
+{
+
+    int n;
+    cin >> n;
+    vector<int> prices(n);
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> prices[i];
+    }
+
+    cout << getMaxProfit(prices);
+
+    return 0;
+}
