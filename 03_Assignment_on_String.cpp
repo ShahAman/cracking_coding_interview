6f2d7748b36e2374b84c7ee5bea2476eaf0b07dc
Shahnewaz <shahnewaz@sgcsoft.net>
2023-08-14 22:11:39 +0600

    Added assignment on string

---
+++ 03_Assignment_on_String.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool isValid(string s) {
+    stack<char> st;
+    unordered_map<char, char> bracket_map = {{')', '('}, {']', '['}, {'}', '{'}};
+
+    for(char c : s){
+        if(bracket_map.find(c) != bracket_map.end()){
+            if(st.empty() || st.top() != bracket_map[c]){
+                return false;
+            }
+            st.pop();
+        }else{
+            st.push(c);
+        }
+    }
+
+    return st.empty();
+}
+
+int main(){
+    string s;
+    cin >> s;
+    if(isValid(s)){
+        cout << "true" << endl;
+    }else{
+        cout << "false" << endl;
+    }
+
+    return 0;
+}
